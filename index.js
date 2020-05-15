b7c7ad1c3b29e5666f6dca38b84492b33c13fc24
daltlc <daltonlcarr@yahoo.com>
2020-05-15 16:46:38 -0700

    basic node server/api that will provide an endpoint for us to access tweets from Stocktwitz

---
+++ index.js
@@ -0,0 +1,29 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+var express = require('express');
+var app = express();
+
+var params = {
+	screen_name: 'Stocktwits',
+	count: 12,
+	lang: 'en'
+};
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
+app.get('/api', (req, res) => {
+	T.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			res.json(tweets);
+		} else {
+			console.log(error);
+		}
+	});
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('Server started on PORT: 8080');
